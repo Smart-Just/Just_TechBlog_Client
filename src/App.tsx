73fc60acd24993af1f96dea85092921cd57bc88e
nGyu <vpdls1511@gmail.com>
2022-11-07 21:11:31 +0900

    feat(vpdls1511) : 라우터 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
+import {useRoutes} from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return useRoutes([
+    {
+      path: '/' ,
+      element : <MainLayout />,
+      children: [
+        {path: '', element: <Home />}
+      ]
+    }
+  ])
 }
 
 export default App;
